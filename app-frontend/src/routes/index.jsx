a36c34f42ac8d7e8e7b41bca379c7a6f5116a3a9
moi <irf630b@proton.me>
2023-10-28 03:19:19 -0400

    initial commit

--- app-frontend/src/routes/index.jsx
+++ app-frontend/src/routes/index.jsx
@@ -13,17 +13,12 @@ const DogsDetails = React.lazy(() => import("@/pages/Dogs/DogsDetails"));
 const DogsSearch = React.lazy(() => import("@/pages/Dogs/DogsSearch"));
 const DogsCreate = React.lazy(() => import("@/pages/Dogs/DogsCreate"));
 
-// Paginas de errores genericos
-const Error404 = React.lazy(() => import("@/pages/utils/Error404"));
-const Error500 = React.lazy(() => import("@/pages/utils/Error500"));
-
 const routes = [
   { path: "/",        element: <Home />,},
   { path: "/home",    element: <DashboardLayout Component={DogsIndex} /> },
   { path: "/search",  element: <DashboardLayout Component={DogsSearch} />},
   { path: "/create",  element: <DashboardLayout Component={DogsCreate} />},
   { path: "/details", element: <DashboardLayout Component={DogsDetails} />},
-  { path: "/*",       element: <Error404 />}, // para rutas no definidas
 ];
 
 const router = createBrowserRouter(routes);
