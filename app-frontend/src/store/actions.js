1f1e0a268c2958d44c17fcb576d3eed290094a8b
moi <irf630b@proton.me>
2023-10-31 01:15:37 -0400

    guardado de preferencias sobre el ordenado y filtrado de elementos en el estado global

--- app-frontend/src/store/actions.js
+++ app-frontend/src/store/actions.js
@@ -11,11 +11,24 @@ export const getAllDogs = () => {
   }
 };
 
-
 export const getDogsByName = (name) => {
   return async function (dispatch){
     var dogsList = await api.search_by_name(name);
     return dispatch({ type: "SET_DOGS_LIST", payload: dogsList });
+  }
+};
+
+export const setFilteringState = (prefered_filter) => {
+  return async function (dispatch){
+    var dogsList = await api.search_by_name(name);
+    return dispatch({ type: "SET_LIST_FILTER", payload: prefered_filter });
+  }
+};
+
+export const setOrderingState = (prefered_order) => {
+  return async function (dispatch){
+    var dogsList = await api.search_by_name(name);
+    return dispatch({ type: "SET_LIST_ORDER", payload: prefered_order });
   }
 };
 
--- app-frontend/src/store/rootReducer.js
+++ app-frontend/src/store/rootReducer.js
@@ -1,7 +1,11 @@
 /*-----------------------------------------------------------------------------------------*/
 /* Initial State. */
 /*-----------------------------------------------------------------------------------------*/
-const initialState = {"database": [], "api": []};
+const initialState = {
+  prefered_order:{"type": "", "way": ""},
+  prefered_filter:{"origin": [], "temperament": []},
+  list:{"database": [], "api": []}
+};
 
 /*-----------------------------------------------------------------------------------------*/
 /* Reducers. */
@@ -10,7 +14,15 @@ const rootReducer = (currentState = initialState, action) => {
   switch (action.type){
 
     case "SET_DOGS_LIST":{
-      return action.payload;
+      return {...currentState, list:action.payload};
+    }
+
+    case "SET_LIST_FILTER":{
+      return {...currentState, prefered_filter:action.payload};
+    }
+
+    case "SET_LIST_ORDER":{
+      return {...currentState, prefered_order:action.payload};
     }
 
     default: {
